79459c09d1cb59511c6e4b9acfedf594bb98bc5e
JaeBum Lee <55440637+wo9374@users.noreply.github.com>
2023-06-19 23:17:26 +0900

    Initial commit

---
+++ app/src/main/java/com/example/androidtechnotecompose/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.androidtechnotecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+import com.example.androidtechnotecompose.navigation.TechNoteNavHost
+import com.example.androidtechnotecompose.ui.theme.TechNoteComposeTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/androidtechnotecompose/ui/theme/Theme.kt
@@ -0,0 +1,64 @@
+package com.example.androidtechnotecompose.ui.theme
+
+import android.os.Build
+import android.util.Log
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
+
+@Composable
+fun TechNoteComposeTheme(
+    isDarkTheme: Boolean = isSystemInDarkTheme(),
+    isDynamicColor: Boolean = true, // Dynamic color is available on Android 12+
+    content: @Composable () -> Unit
+) {
+    val dynamicColor = isDynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+
+    Log.d("다크모드", "dynamicColor : $dynamicColor")
+    Log.d("다크모드", "isDarkTheme : $isDarkTheme")
+
+    val colorScheme = when {
+        dynamicColor && isDarkTheme -> {
+            dynamicDarkColorScheme(LocalContext.current)
+        }
+        dynamicColor && !isDarkTheme -> {
+            dynamicLightColorScheme(LocalContext.current)
+        }
+        isDarkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    Log.d("다크모드", "colorScheme : $colorScheme")
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
