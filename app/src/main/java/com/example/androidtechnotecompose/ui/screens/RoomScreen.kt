ed5d564c582e61dafe30e6e8ac5add70f7a9cfc5
JaeBum Lee <wo9374@naver.com>
2023-07-04 11:42:30 +0900

    refactor: 추가 삭제 기능을 위한 Layout 수정

--- app/src/main/java/com/example/androidtechnotecompose/ui/screens/RoomScreen.kt
+++ app/src/main/java/com/example/androidtechnotecompose/ui/screens/RoomScreen.kt
@@ -1,45 +1,99 @@
+@file:OptIn(
+    ExperimentalMaterial3Api::class,
+    ExperimentalMaterialApi::class,
+    ExperimentalMaterial3Api::class
+)
+
 package com.example.androidtechnotecompose.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Icon
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetValue
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.rememberModalBottomSheetState
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.androidtechnotecompose.viewmodel.RoomViewModel
 
 @Composable
 fun RoomScreen(
-    viewModel: RoomViewModel = hiltViewModel()
-){
-    val list by viewModel.numbers.collectAsState()
-    val scrollState = rememberLazyListState()
-    //val scope = rememberCoroutineScope()
+    roomViewModel: RoomViewModel = hiltViewModel()
+) {
+    val coroutineScope = rememberCoroutineScope()
 
-    LazyColumn(
-        state = scrollState,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        items(
-            items = list
-        ){
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp)
-            ) {
-                Text(
-                    text = it.value.toString()
-                )
+    val list by roomViewModel.numbers.collectAsState()
+    val scrollState = rememberLazyListState()
+
+    Box(modifier = Modifier.fillMaxSize()) {
+
+        LazyColumn(
+            state = scrollState,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            items(items = list) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(50.dp)
+                ) {
+                    Text(
+                        text = it.value.toString()
+                    )
+                }
+            }
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.DarkGray)
+                .align(Alignment.BottomCenter),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+
+            val textState = remember { mutableStateOf("") }
+            TextField(
+                value = textState.value,
+                onValueChange = { textValue ->
+                    textState.value = textValue
+                },
+                placeholder = {
+                    Text(text = "추가할 Text")
+                }
+            )
+
+            Button(onClick = {}) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "추가")
             }
         }
     }
