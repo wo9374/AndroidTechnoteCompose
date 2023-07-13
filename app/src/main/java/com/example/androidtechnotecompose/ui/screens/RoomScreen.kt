c05ae028da33c380a22fa0a309bd56eea1e9c6d6
JaeBum Lee <wo9374@naver.com>
2023-07-14 01:15:21 +0900

    refator: Room 추가 버튼 keyboard action으로 대체

--- app/src/main/java/com/example/androidtechnotecompose/ui/screens/RoomScreen.kt
+++ app/src/main/java/com/example/androidtechnotecompose/ui/screens/RoomScreen.kt
@@ -3,7 +3,6 @@
 package com.example.androidtechnotecompose.ui.screens
 
 import android.widget.Toast
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,25 +11,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.ThumbUp
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
@@ -48,6 +45,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -66,15 +66,14 @@ fun RoomScreen(
     val list by roomViewModel.numbers.collectAsState()
     val scrollState = rememberLazyListState()
 
-    val textFieldHeight = 60.dp
-
-    Box(modifier = Modifier.fillMaxSize()) {
+    Column(modifier = Modifier.fillMaxSize()) {
 
         LazyColumn(
             state = scrollState,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(top = 4.dp, bottom = textFieldHeight)
+                .weight(1f)
+                .padding(vertical = 4.dp)
         ) {
             items(items = list) {
 
@@ -116,50 +115,48 @@ fun RoomScreen(
             }
         }
 
-        Row(
+
+        val textState = remember { mutableStateOf("") }
+        val focusManager = LocalFocusManager.current
+
+        OutlinedTextField(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(textFieldHeight)
-                .background(Color.DarkGray)
-                .align(Alignment.BottomCenter),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ) {
-
-            val textState = remember { mutableStateOf("") }
-
-            val focusManager = LocalFocusManager.current
-
-            OutlinedTextField(
-                value = textState.value,
-                onValueChange = { textValue ->
-                    textState.value = textValue
-                },
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Default.ThumbUp,
-                        contentDescription = "Email Icon"
-                    )
-                },
-                placeholder = {
-                    Text(text = stringResource(R.string.add_text))
-                }
-            )
-
-            Button(onClick = {
+                .wrapContentHeight(),
+            value = textState.value,
+            onValueChange = { textValue ->
+                textState.value = textValue
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.ThumbUp,
+                    contentDescription = "Email Icon",
+                    tint = MaterialTheme.colors.primary
+                )
+            },
+            placeholder = {
+                Text(text = stringResource(R.string.add_text))
+            },
+            colors = TextFieldDefaults.textFieldColors(textColor = Color.LightGray),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                capitalization = KeyboardCapitalization.Sentences,
+                autoCorrect = true,
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(onDone = {
                 if (textState.value.isNotEmpty()) {
                     coroutineScope.launch {
                         roomViewModel.insertItem(textState.value)
                         textState.value = ""
                         focusManager.clearFocus()
+                        scrollState.animateScrollToItem(list.lastIndex)
                     }
                 } else {
                     Toast.makeText(context, "추가할 Text를 입력해주세요.", Toast.LENGTH_SHORT).show()
                 }
-            }) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = "추가")
-            }
-        }
+            }),
+        )
     }
 }
 
